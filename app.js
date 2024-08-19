9c4c2740126b92f51e166aea7a73feaa06383bf2
Marine CHAMOUX <marine.chamoux@PAR-4300-LA.aldebaran.lan>
2024-08-19 22:31:01 +0200

    Use cors to listen to front-end

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var path = require('path');
 require('dotenv').config();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gameRouter = require('./routes/game');
 
 var app = express();
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL, // Remplacez par l'origine de votre front-end
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
