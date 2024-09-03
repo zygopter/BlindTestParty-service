5d33b6701ebdd50145715f6e1019789cfcd81825
Marine CHAMOUX <marine.chamoux@PAR-4300-LA.aldebaran.lan>
2024-09-03 22:44:04 +0200

    Allow more possible origins

--- app.js
+++ app.js
@@ -11,11 +11,30 @@ var gameRouter = require('./routes/game');
 
 var app = express();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL, // Remplacez par l'origine de votre front-end
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const allowedOrigins = [
+    'https://blindtest-party.netlify.app',
+    'https://main--blindtest-party.netlify.app',
+    'http://localhost:3001'
+  ];
+  
+  app.use(cors({
+    origin: function(origin, callback) {
+      // Allow requests with no origin like mobile apps or curl
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // If you need to include cookies in requests
+  }));
+
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL, // Remplacez par l'origine de votre front-end
+//     methods: ['GET', 'POST'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// }));
 
 app.use(logger('dev'));
 app.use(express.json());
